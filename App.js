41e14b409d0f5ae51ace69dd3cd3adcb59909eaf
Nicholas Agwah <nagwah@teqbridgeltd.com>
2023-07-04 15:43:12 +0100

    first series

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [name, setName] = useState("Nicolas");
+	const [age, setAge] = useState(0);
+
+	const handleName = (e) => {
+		setName(e);
+	};
+	const handleAge = (e) => {
+		setAge(e);
+	};
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.boldText}>
+					Hello {name} is {age} years{" "}
+				</Text>
+			</View>
+			<Text>Enter name</Text>
+			<TextInput
+				multiline
+				style={styles.input}
+				placeholder="Enter Name"
+				onChangeText={handleName}
+			/>
+			<Text>Enter Age</Text>
+			<TextInput
+				keyboardType="numeric"
+				style={styles.input}
+				placeholder="Enter Age"
+				onChangeText={handleAge}
+			/>
+
+			<View style={styles.buttonContainer}>
+				<Button title="Update State" />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	header: {
+		backgroundColor: "red",
+		padding: 20,
+	},
+	boldText: {
+		fontWeight: "bold",
+		color: "white",
+	},
+	buttonContainer: {
+		marginTop: 20,
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: "#777",
+		margin: 10,
+		padding: 10,
+		width: 200,
+	},
 });
