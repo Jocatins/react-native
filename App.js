b062a17adcc06d4333a2c5579fd300613e500b99
Nicholas Agwah <nagwah@teqbridgeltd.com>
2023-07-05 14:54:17 +0100

    keyboard dismissal

--- App.js
+++ App.js
@@ -2,56 +2,74 @@ import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
 	StyleSheet,
-	Text,
 	View,
-	ScrollView,
 	FlatList,
-	TouchableOpacity,
+	Alert,
+	TouchableWithoutFeedback,
+	Keyboard,
 } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
-	const [people, setPeople] = useState([
-		{ name: "shaun", id: "1" },
-		{ name: "yoshi", id: "2" },
-		{ name: "mario", id: "3" },
-		{ name: "luigi", id: "4" },
-		{ name: "peach", id: "5" },
-		{ name: "toad", id: "6" },
-		{ name: "titan", id: "7" },
-		{ name: "titans", id: "8" },
-		{ name: "phil", id: "9" },
+	const [todos, setTodos] = useState([
+		{ text: "buy coffee", key: "1" },
+		{ text: "create an app", key: "2" },
+		{ text: "play on the switch", key: "3" },
 	]);
 
 	const pressHandler = (id) => {
-		console.log(id);
-		setPeople((prev) => {
-			return prev.filter((item) => item.id != id);
+		setTodos((prev) => {
+			return prev.filter((todo) => todo.key != id);
 		});
 	};
 
+	const submitHandler = (text) => {
+		if (text.length > 5) {
+			setTodos((prev) => {
+				return [{ text: text, key: Math.random().toString() }, ...prev];
+			});
+		} else {
+			Alert.alert(
+				"Error",
+				"Text should be at least five characters long",
+				[
+					{
+						text: "Understood",
+						onPress: () => console.log("Alert closed"),
+					},
+				]
+			);
+		}
+	};
+
 	return (
-		<View style={styles.container}>
-			<FlatList
-				numColumns={3}
-				keyExtractor={(item) => item.id}
-				data={people}
-				renderItem={({ item }) => (
-					<TouchableOpacity onPress={() => pressHandler(item.id)}>
-						<Text style={styles.item}>{item.name}</Text>
-					</TouchableOpacity>
-				)}
-			/>
-
-			{/* <ScrollView>
-				{people.map((people) => (
-					<View key={people.id}>
-						<Text style={styles.item}>{people.name}</Text>
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}
+		>
+			<View style={styles.container}>
+				<Header />
+				<View style={styles.content}>
+					<AddTodo submitHandler={submitHandler} />
+					<View style={styles.list}>
+						<FlatList
+							data={todos}
+							renderItem={({ item }) => (
+								<TodoItem
+									item={item}
+									pressHandler={pressHandler}
+								/>
+							)}
+						/>
 					</View>
-				))}
-			</ScrollView> */}
+				</View>
 
-			<StatusBar style="auto" />
-		</View>
+				<StatusBar style="auto" />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
@@ -59,17 +77,11 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
-		paddingTop: 40,
-		paddingHorizontal: 20,
-		// alignItems: "center",
-		// justifyContent: "center",
 	},
-	item: {
-		marginTop: 24,
-		padding: 30,
-		backgroundColor: "red",
-		fontSize: 20,
-		color: "white",
-		marginHorizontal: 10,
+	content: {
+		padding: 40,
+	},
+	list: {
+		padding: 40,
 	},
 });
